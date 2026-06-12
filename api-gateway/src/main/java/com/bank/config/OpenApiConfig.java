197b6cfef07c4383cfc6e4ccb8e6fbdbfdbd80ff
TL <ram@gmail.com>
2026-06-12 16:06:30 +0530

    complete api gateway

---
+++ api-gateway/src/main/java/com/bank/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI bankingAPI() {
+		
+		return new OpenAPI()
+				.info(new Info()
+						.title("Banking Microservices Api")
+						.version("1.0")
+						.description(
+								"Banking Application using Spring Boot Microservices")
+						.contact(new Contact()
+								.name("P.Ramachandra Rao")
+								.email("peetharamachandrarao@gmail.com")
+								)
+						);
+	}
+
+}
