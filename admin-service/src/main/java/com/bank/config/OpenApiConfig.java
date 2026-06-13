999ed143d594c7657c8ee1d5cb19d9eac273c369
TL <ram@gmail.com>
2026-06-13 15:54:06 +0530

    completed admin service

--- admin-service/src/main/java/com/bank/config/OpenApiConfig.java
+++ admin-service/src/main/java/com/bank/config/OpenApiConfig.java
@@ -3,17 +3,22 @@ package com.bank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
 	
+	
 	@Bean
 	public OpenAPI bankingAPI() {
+		
+		final String securitySchemeName = "bearerAuth";
 		
 		return new OpenAPI()
 				.info(new Info()
@@ -23,18 +28,19 @@ public class OpenApiConfig {
 								"Banking Application Using Spring Boot Microservices")
 						.contact(new Contact()
 								.name("P.Ramachandra Rao")
-								.email("peetharamachandrarao@gmail.com")
-								)
-						)
+								.email("peetharamachandrarao@gmail.com")))
+				.addSecurityItem(
+						new SecurityRequirement()
+						.addList(securitySchemeName))
 				
 				.components(
 						new Components()
-						.addSecuritySchemes("bearerAuth",
-								new SecurityScheme()
+						.addSecuritySchemes(securitySchemeName, new SecurityScheme()
+								.name("Authorization")
 								.type(SecurityScheme.Type.HTTP)
 								.scheme("bearer")
-								.bearerFormat("JWT")
-								)
+								.bearerFormat("JWT"))
+						
 						);
 				
 	}
