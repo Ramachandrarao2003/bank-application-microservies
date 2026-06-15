4dbb26b0f6bd2145e78f523cdc5b17a0e4299781
TL <ram@gmail.com>
2026-06-15 12:02:03 +0530

    Completed Admin Service

--- admin-service/src/main/java/com/bank/config/OpenApiConfig.java
+++ admin-service/src/main/java/com/bank/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.bank.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -21,11 +24,13 @@ public class OpenApiConfig {
 		final String securitySchemeName = "bearerAuth";
 		
 		return new OpenAPI()
+				.servers(List.of(
+						new Server().url("http://localhost:9090")))
 				.info(new Info()
 						.title("Banking Microservices API")
 						.version("1.0")
 						.description(
-								"Banking Application Using Spring Boot Microservices")
+								"Admin Management APIs")
 						.contact(new Contact()
 								.name("P.Ramachandra Rao")
 								.email("peetharamachandrarao@gmail.com")))
