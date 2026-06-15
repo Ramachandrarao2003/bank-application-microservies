97adcf469875ad525ed74e7f8d7bed2bad2be0f8
TL <ram@gmail.com>
2026-06-15 11:59:17 +0530

    Completed Auth register & Login Service

--- auth-service/src/main/java/com/bank/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/bank/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.bank.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,11 +19,13 @@ public class OpenApiConfig {
 	public OpenAPI bankingApi() {
 		
 		return new OpenAPI()
+				.servers(List.of(
+						new Server().url("http://localhost:9090")))
 				.info(new Info()
 						.title("Banking Microservices API")
 						.version("1.0")
 						.description(
-								"Banking Application Using Spring Boot Microservices")
+								"Register and Login Management APIs")
 						.contact(
 								new Contact()
 								.name("P.Ramachandra Rao")
