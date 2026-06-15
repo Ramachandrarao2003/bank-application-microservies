302edff4d809c7d3c6fa3f1348b72847bfa9b3f1
TL <ram@gmail.com>
2026-06-15 11:57:44 +0530

    completed Api Gateway service

--- api-gateway/src/main/java/com/bank/config/OpenApiConfig.java
+++ api-gateway/src/main/java/com/bank/config/OpenApiConfig.java
@@ -1,11 +1,14 @@
 package com.bank.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +17,9 @@ public class OpenApiConfig {
 	public OpenAPI bankingAPI() {
 		
 		return new OpenAPI()
+				.servers(List.of(
+						new Server().url("http://localhost:9090")
+						.description("API Gateway")))
 				.info(new Info()
 						.title("Banking Microservices Api")
 						.version("1.0")
---
+++ api-gateway/src/main/java/com/bank/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		
+		return http
+				.csrf(ServerHttpSecurity.CsrfSpec::disable)
+				.authorizeExchange(exchange -> exchange
+						.pathMatchers(
+								"/swagger-ui.html",
+								"/swagger-ui/**",
+								"/v3/api-docs/**",
+								"/auth-service/**",
+								"/account-service/**",
+								"/transaction-service/**",
+								"/admin-service/**")
+						.permitAll()
+						.anyExchange()
+						.permitAll())
+				.build();
+	}
+
+}
